db9d7a4ab94af43f3d85f3d1b73b97c12759842d
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-05-31 14:52:47 -0700

    FizzBuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    """Print numbers 1 to 100.
+
+    Print Fizz in place of number for multiples of 3.
+    Print Buzz in place of number for multiples of 5.
+    Print FizzBuzz in place of number for multiples of 3 and 5.
+    """
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end="")
+        elif i % 3 == 0:
+            print("Fizz", end="")
+        elif i % 5 == 0:
+            print("Buzz", end="")
+        else:
+            print("{} ".format(i), end="")
