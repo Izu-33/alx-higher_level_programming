d287676c41f9dd6a605c53990e7749683da03cd7
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-06-09 08:57:05 -0700

    Update dictionary name

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if my_dict is None or my_dict == {}:
+    if a_dictionary is None or a_dictionary == {}:
         return None
     return max(a_dictionary, key=lambda key: a_dictionary[key])
