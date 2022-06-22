b40cd8c6c74ed302411d02e53db4b7378a02f14b
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-06-22 00:10:36 -0700

    Updated `try` statement with `else` clause

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    """Print the first x integer elements of a list.
-
-    Args:
-        my_list (list): The list to print elements from.
-        x (int): The number of elements to print.
-
-    Returns:
-        The number of elements printed.
-    """
     num = 0
     for i in my_list[:x]:
         try:
             print("{:d}".format(i), end="")
-            num += 1
         except (ValueError, TypeError):
             continue
+        else:
+            num += 1
     print()
     return num
