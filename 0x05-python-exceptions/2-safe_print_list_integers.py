f4e1dcdb24603989d108da7e974b4780b0527e87
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-06-21 23:25:58 -0700

    Removed hanlde for `IndexError`

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,5 @@ def safe_print_list_integers(my_list=[], x=0):
             num += 1
         except (ValueError, TypeError):
             continue
-        except IndexError as err:
-            print('Handling run-time error:', err)
     print()
     return num
