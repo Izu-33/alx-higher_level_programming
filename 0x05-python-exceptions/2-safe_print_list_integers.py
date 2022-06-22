5d38bdab7479dc2684ea83a0a90322b6414690e1
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-06-21 23:23:43 -0700

    Update script with `ValueError` catch

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,13 +3,12 @@
 def safe_print_list_integers(my_list=[], x=0):
     num = 0
     for i in my_list[:x]:
-        if type(i) == str:
+        try:
+            print("{:d}".format(i), end='')
+            num += 1
+        except (ValueError, TypeError):
             continue
-        else:
-            try:
-                print("{:d}".format(i), end='')
-                num += 1
-            except IndexError as err:
-                print('Handling run-time error:', err)
+        except IndexError as err:
+            print('Handling run-time error:', err)
     print()
     return num
