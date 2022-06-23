a007d8d96b1bac8261bf0395c1b79da706da1098
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-06-23 03:21:19 -0700

    Class definition

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a class Square."""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size):
+        """Initialize a square.
+
+        Args:
+            size (int): Size of new square.
+        """
+        self.__size = size
