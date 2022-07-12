4e151f1f0f78e547fad0cbf8063982af5da935ff
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-07-12 06:37:07 -0700

    Update `Rectangle` class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -105,3 +105,45 @@ class Rectangle(Base):
                                                            self.__y,
                                                            self.__width,
                                                            self.__height)
+
+        def update(self, *args, **kwargs):
+            """Update the rectangle.
+
+            Args:
+                *args (int): Attribute values.
+                **kwargs (dict): key/value pairs of attributes.
+            """
+            if args and len(args) != 0:
+                a = 0
+                for arg in args:
+                    if a == 0:
+                        if arg is None:
+                            self.__init__(self.__width, self.__height,
+                                          self.__x, self.__y)
+                        else:
+                            self.id = arg
+                    elif a == 1:
+                        self.__width = arg
+                    elif a == 2:
+                        self.__height = arg
+                    elif a == 3:
+                        self.__x = arg
+                    elif a == 4:
+                        self.__y = arg
+                    a += 1
+            elif kwargs and len(kwargs) != 0:
+                for k, v in kwargs.items():
+                    if k == "id":
+                        if v is None:
+                            self.__init__(self.__width, self.__height,
+                                          self.__x, self.__y)
+                        else:
+                            self.id = v
+                    elif k == "width":
+                        self.__width = v
+                    elif k == "height":
+                        self.__height = v
+                    elif k == "x":
+                        self.__x = v
+                    elif k == "y":
+                        self.__y = v
