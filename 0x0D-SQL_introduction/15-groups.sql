16dc917b12c5405caddb3867323114d7ab528db0
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-08-09 13:03:19 -0700

    Update `15-groups.sql`

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- ists the number of records with the same score in the table second_table of the database hbtn_0c_0
-SELECT `score`, COUNT(*) AS `average`
+SELECT `score`, COUNT(*) AS `number`
 FROM `second_table`
 GROUP BY `score`
 ORDER BY `number` DESC;
