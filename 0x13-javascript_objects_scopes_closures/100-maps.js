9f0495846861db6cda7743c69e7fe6b39bb40958
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-09-06 23:01:49 +0000

    Update 100

--- 0x13-javascript_objects_scopes_closures/100-maps.js
+++ 0x13-javascript_objects_scopes_closures/100-maps.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
-const array = require('./100-data.js').list;
-
-console.log(array);
-let cont = 0;
-const map1 = array.map(function (x) {
-  return (x * cont++);
-});
-console.log(map1);
+const list = require('./100-data.js').list;
+console.log(list);
+console.log(list.map((item, index) => item * index));
