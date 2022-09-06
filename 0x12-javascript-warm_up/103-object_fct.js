e2ccfb41d5873cf0d22b10cddd6ba3f385afaf68
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-09-06 01:10:45 +0000

    Added advanced tasks

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+let myObject = {
+  type: 'object',
+  value: 1
+};
+console.log(myObject);
+
+myObject.incr = function () {
+  this.value++;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
