19958483fea7b08b12fcc38993acd4de0ee3f0fb
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-09-13 01:28:51 -0700

    Update task 11 file

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -11,8 +11,8 @@ if __name__ = "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    Base.metadata.create_all(engine)
+    session = Session(engine)
 
     louisiana = State(name="Louisiana")
     session.add(louisiana)
