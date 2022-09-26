4c806ad246b7b8c80856966288e2c801e3f8f788
Izundu Dan-Ekeh <dizundu@yahoo.com>
2022-09-26 11:08:14 -0700

    Updated task #8 file

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,7 +9,7 @@ import requests
 if __name__ == "__main__":
     letter = "" if len(sys.argv) == 1 else sys.argv[1]
     payload = {"q": letter}
-    req = requests.get('http://0.0.0.0:5000/search_user', data=payload)
+    req = requests.post('http://0.0.0.0:5000/search_user', data=payload)
     try:
         response = req.json()
         if response = {}:
